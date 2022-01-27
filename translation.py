e1bcb9d6426b709afea100185d50a4dafeb2c1c4
Cooperator-gem <97656070+Cooperator-gem@users.noreply.github.com>
2022-01-27 18:31:28 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -12,7 +12,7 @@ class Translation(object):
 😎I can upload to telegram as File or Video or Audio(if supported) format.
 Click /help for more details!
 
-𝐅𝐨𝐫 𝐦𝐨𝐫𝐞 𝐝𝐞𝐭𝐚𝐢𝐥𝐬 𝐣𝐨𝐢𝐧 𝐨𝐮𝐫 𝐠𝐫𝐨𝐮𝐩 - https://t.me/joinchat/-3Vjw61udNM3OWU9
+<b>Join [Here](https://t.me/joinchat/-3Vjw61udNM3OWU9)</b>
 \n</b>"""
     FORMAT_SELECTION = "<b>✅Select the desired format: <a href='{}'>file size might be approximate.</a> \n🧭If you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\n😎You can use /delthumbnail to delete the auto-generated thumbnail.</b>"
     SET_CUSTOM_USERNAME_PASSWORD = """<b>🤠If you want to download premium videos, provide in the following format:
